713330c79d2de6569dcc4f2a564f0065aada4607
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 23:03:59 +0100

    Update style of hero and header sections

--- src/components/header/HeaderSection.vue
+++ src/components/header/HeaderSection.vue
@@ -1,24 +1,39 @@
 <template lang="">
-  <header class="bg-nude flex justify-around items-center align-center p-8">
-    <div class="text-3xl uppercase">
+  <header class="bg-nude flex items-center align-center p-8">
+    <div class="text-3xl uppercase container mx-auto">
       <h2>True<span class="font-extrabold">Buy </span></h2>
     </div>
 
-    <nav class="basis-1/4 ">
+    <nav class="">
       <ul class="flex items-center justify-between">
-        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">HomePage</li>
-        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">About Us</li>
-        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">Categories</li>
+        <li
+          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+        >
+          HomePage
+        </li>
+        <li
+          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+        >
+          About Us
+        </li>
+        <li
+          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+        >
+          Categories
+        </li>
       </ul>
     </nav>
 
     <!-- TODO add userprofile icon -->
   </header>
+  <HeroSection></HeroSection>
 </template>
 <script>
+import HeroSection from "./HeroSection";
 
 export default {
   name: "HeaderSection",
+  components: { HeroSection },
 };
 </script>
 <style lang=""></style>
--- src/components/header/HeroSection.vue
+++ src/components/header/HeroSection.vue
@@ -1,18 +1,28 @@
 <template lang="">
-  <section>
-    <div>
-      <div>
-        <p>Now, furniture shopping is simple</p>
-        <input
-          placeholder="Search your product here"
-          type="text"
-          name=""
-          id=""
-        />
-        <ButtonUI> Search </ButtonUI>
+  <section class="hero-section pt-12 h-full">
+    <div class="flex container mx-auto">
+      <div class="basis-1/4 flex flex-col">
+        <p class="font-bold text-4xl capitalize mb-6">
+          Now, furniture shopping is simple
+        </p>
+
+        <div class="mb-4">
+          <input
+            class="p-4"
+            placeholder="Search your product here"
+            type="text"
+            name=""
+            id=""
+          />
+
+          <ButtonUI class="rounded rounded-sm bg-black text-white p-4">
+            Search
+          </ButtonUI>
+        </div>
       </div>
-      <div>
-        <p>
+
+      <div class="basis-1/4">
+        <p class="text-lg tracking-wide">
           We've got the furniture you need to fill your new HomePage. Browse now
           for free and get big discounts on everything from tables to lamps!
         </p>
@@ -28,4 +38,12 @@ export default {
   components: { ButtonUI },
 };
 </script>
-<style lang=""></style>
+<style>
+.hero-section {
+  height: 40%;
+  background-image: url("/src/assets/hero-bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: top;
+}
+</style>

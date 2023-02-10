26a8c31a25ba8bcb298359ec7063d2b60e20643b
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 20:07:34 +0100

    Create and style HeaderSection component

---
+++ src/components/header/HeaderSection.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <header class="bg-nude flex justify-around items-center align-center p-8">
+    <div class="text-3xl uppercase">
+      <h2>True<span class="font-extrabold">Buy </span></h2>
+    </div>
+
+    <nav class="basis-1/4 ">
+      <ul class="flex items-center justify-between">
+        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">HomePage</li>
+        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">About Us</li>
+        <li class="text-xl hover:underline  decoration-green100 decoration-4 underline-offset-4">Categories</li>
+      </ul>
+    </nav>
+
+    <!-- TODO add userprofile icon -->
+  </header>
+</template>
+<script>
+
+export default {
+  name: "HeaderSection",
+};
+</script>
+<style lang=""></style>
---
+++ src/components/header/HeroSection.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <section>
+    <div>
+      <div>
+        <p>Now, furniture shopping is simple</p>
+        <input
+          placeholder="Search your product here"
+          type="text"
+          name=""
+          id=""
+        />
+        <ButtonUI> Search </ButtonUI>
+      </div>
+      <div>
+        <p>
+          We've got the furniture you need to fill your new HomePage. Browse now
+          for free and get big discounts on everything from tables to lamps!
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import ButtonUI from "../UI/ButtonUI";
+
+export default {
+  name: "HeroSection",
+  components: { ButtonUI },
+};
+</script>
+<style lang=""></style>

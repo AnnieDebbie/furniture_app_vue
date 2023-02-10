eaaa1ad7190d0a527ccb37d6f1fde3fee209c004
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 23:14:57 +0100

    Create and style footer component Add footer to HomePage

---
+++ src/components/footer/FooterSection.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  <footer class="mt-4 bg-nude flex p-4">
+    <div>
+      <ul>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Blog</li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li>Contact Us</li>
+        <li>Support</li>
+        <li>Live chat</li>
+      </ul>
+    </div>
+    <div><input type="text" /><ButtonUI>Subscribe</ButtonUI></div>
+  </footer>
+</template>
+<script>
+import ButtonUI from '../UI/ButtonUI.vue';
+export default {
+  name: "FooterSection",
+  components: { ButtonUI },
+};
+</script>
+<style lang=""></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,12 +3,14 @@
     <HeaderSection></HeaderSection>
     <PopularCategories/>
     <WhyUs/>
+    <FooterSection/>
   </div>
 </template>
 <script>
 import HeaderSection from '../components/header/HeaderSection.vue';
 import PopularCategories from '../components/main/products/PopularCategories.vue';
 import WhyUs from "../components/main/WhyUs.vue"
+import FooterSection from "../components/footer/FooterSection.vue"
 
 export default {
   name: "HomePage",
@@ -16,6 +18,7 @@ export default {
     HeaderSection,
     PopularCategories,
     WhyUs,
+    FooterSection,
   },
 };
 </script>

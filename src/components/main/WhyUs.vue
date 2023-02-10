9689beed9024ce2e3453e4c617823a14780438de
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 23:02:49 +0100

    Create and style WhyUS component

---
+++ src/components/main/WhyUs.vue
@@ -0,0 +1,42 @@
+<template lang="">
+  <section class="flex container mx-auto">
+    <div class="flex flex-col gap-4 p-4">
+      <h3 class="font-bold text-lg">A Modern Home Starts with Truebuy.</h3>
+      <p class="">
+        You'll find the perfect home furnishings without breaking your budget.
+      </p>
+      <img src="@/assets/couch.png" alt="A couch" />
+    </div>
+
+    <div class="bg-nude p-4">
+      <ul class="space-y-3">
+        <li>
+          <div class="flex gap-4 items-center mb-2">
+            <img src="@/assets/check-circle-rounded.svg" alt="" />
+            <h4 class="font-bold text-lg">A Customized Experience for Every Style</h4>
+          </div>
+          <p>
+            Our customizing tool allows you to filter by size, color, type of
+            furniture and more!
+          </p>
+        </li>
+        <li>
+          <div class="flex gap-4 items-center mb-2">
+            <img src="@/assets/check-circle-rounded.svg" alt="" />
+            <h4 class="font-bold text-lg">Competitive Prices</h4>
+          </div>
+          <p>
+            The majority of our products are priced at under $1000 - some as low
+            as $89!
+          </p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "WhyUs",
+};
+</script>
+<style lang=""></style>

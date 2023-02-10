2dc678ce2ae6fbeff5ab5eeb2f6847fa056085f9
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 23:04:31 +0100

    Create HomePage component

---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <div>
+    <HeaderSection></HeaderSection>
+    <PopularCategories/>
+    <WhyUs/>
+  </div>
+</template>
+<script>
+import HeaderSection from '../components/header/HeaderSection.vue';
+import PopularCategories from '../components/main/products/PopularCategories.vue';
+import WhyUs from "../components/main/WhyUs.vue"
+
+export default {
+  name: "HomePage",
+  components: {
+    HeaderSection,
+    PopularCategories,
+    WhyUs,
+  },
+};
+</script>
+
+<style lang=""></style>

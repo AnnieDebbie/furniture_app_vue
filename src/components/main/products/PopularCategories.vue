d0df50693dde92c30a2c59eec65fcfba6560feed
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-10 23:01:53 +0100

    Create and style PopularCategories component

---
+++ src/components/main/products/PopularCategories.vue
@@ -0,0 +1,52 @@
+<template lang="">
+  <section class="container mx-auto">
+    <h4 class="font-bold text-3xl">Popular Categories</h4>
+
+    <div class="flex align-center items-center">
+      <div v-for="item in popularCategories" :key="item.name">
+        <img  :alt="item.image.alt" :src="item.image.source" />
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "PopularCategories",
+
+  data() {
+    return {
+      popularCategories: [
+        {
+          name: "chair",
+          image: {
+            source: require("@/assets/chair.png"),
+            alt: "A chair",
+          },
+        },
+        {
+          name: "cabinet",
+          image: {
+            source: require("@/assets/cabinet.png"),
+            alt: "A cabinet",
+          },
+        },
+        {
+          name: "lamp",
+          image: {
+            source: require("@/assets/lamp.png"),
+            alt: "A lamp",
+          },
+        },
+        {
+          name: "table",
+          image: {
+            source: require("@/assets/table.png"),
+            alt: "A table",
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang=""></style>

ae82761bc403dfcc92e4698a968c6a3279b17a07
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-11 12:50:27 +0100

    Update HeroSection component -Add placeholder -Change background image -Add line height to texts

--- src/components/header/HeroSection.vue
+++ src/components/header/HeroSection.vue
@@ -1,14 +1,16 @@
 <template lang="">
-  <section class="hero-section pt-12 h-full">
-    <div class="flex container mx-auto">
+  <section class="hero-section pt-16 mb-12">
+    <div class="flex justify-between container mx-auto">
       <div class="basis-1/4 flex flex-col">
-        <p class="font-bold text-4xl capitalize mb-6">
+        <p
+          class="font-bold text-4xl capitalize mb-6 tracking-wide leading-[100%]"
+        >
           Now, furniture shopping is simple
         </p>
 
         <div class="mb-4">
           <input
-            class="p-4"
+            class="p-4 placeholder:text-black"
             placeholder="Search your product here"
             type="text"
             name=""
@@ -22,8 +24,8 @@
       </div>
 
       <div class="basis-1/4">
-        <p class="text-lg tracking-wide">
-          We've got the furniture you need to fill your new HomePage. Browse now
+        <p class="text-xl tracking-wide leading-[150%] font-light">
+          We've got the furniture you need to fill your new Home. Browse now
           for free and get big discounts on everything from tables to lamps!
         </p>
       </div>
@@ -40,10 +42,10 @@ export default {
 </script>
 <style>
 .hero-section {
-  height: 40%;
-  background-image: url("/src/assets/hero-bg.png");
+  height: 891px;
+  background-image: url("/src/assets/hero-bg.jpg");
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: top;
+  background-position: center center;
 }
 </style>

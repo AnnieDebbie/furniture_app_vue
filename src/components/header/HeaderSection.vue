44a49ec6026cec7969055ff58cb5fa557690e360
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-11 12:48:53 +0100

    Update header section - Center items - Remove herosection component

--- src/components/header/HeaderSection.vue
+++ src/components/header/HeaderSection.vue
@@ -1,39 +1,40 @@
 <template lang="">
-  <header class="bg-nude flex items-center align-center p-8">
-    <div class="text-3xl uppercase container mx-auto">
-      <h2>True<span class="font-extrabold">Buy </span></h2>
-    </div>
+  <section class="bg-nude p-12">
+    <header class="container mx-auto flex items-center align-center  justify-between">
+      <div class="text-3xl uppercase">
+        <h2>True<span class="font-extrabold">Buy </span></h2>
+      </div>
 
-    <nav class="">
-      <ul class="flex items-center justify-between">
-        <li
-          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
-        >
-          HomePage
-        </li>
-        <li
-          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
-        >
-          About Us
-        </li>
-        <li
-          class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
-        >
-          Categories
-        </li>
-      </ul>
-    </nav>
+      <nav class="basis-1/4">
+        <ul class="flex items-center justify-between">
+          <li
+            class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+          >
+            Home 
+          </li>
+          <li
+            class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+          >
+            About Us
+          </li>
+          <li
+            class="text-xl hover:underline decoration-green100 decoration-4 underline-offset-4"
+          >
+            Categories
+          </li>
+        </ul>
+      </nav>
 
-    <!-- TODO add userprofile icon -->
-  </header>
-  <HeroSection></HeroSection>
+      <!-- TODO add userprofile icon -->
+    </header>
+  </section>
 </template>
 <script>
-import HeroSection from "./HeroSection";
 
 export default {
   name: "HeaderSection",
-  components: { HeroSection },
+  components: { },
 };
 </script>
-<style lang=""></style>
+<style lang="">
+  </style>

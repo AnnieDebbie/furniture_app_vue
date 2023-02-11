ecd392b93812f2607b35a1df7e073057b4ae2430
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-11 18:39:52 +0100

    Style WhyUs component - Add more spacing - Center items - Increase font size and weight

--- src/components/main/WhyUs.vue
+++ src/components/main/WhyUs.vue
@@ -1,31 +1,33 @@
 <template lang="">
-  <section class="flex container mx-auto">
-    <div class="flex flex-col gap-4 p-4">
-      <h3 class="font-bold text-lg">A Modern Home Starts with Truebuy.</h3>
-      <p class="">
+  <section class="flex justify-evenly  container mx-auto">
+    <div class="basis-1/4 flex flex-col gap-4 pr-8 py-8 ">
+      <h3 class="font-bold text-2xl">A Modern Home Starts with Truebuy.</h3>
+      <p class="leading-[150%] font-light">
         You'll find the perfect home furnishings without breaking your budget.
       </p>
       <img src="@/assets/couch.png" alt="A couch" />
     </div>
 
-    <div class="bg-nude p-4">
-      <ul class="space-y-3">
+    <div class="basis-1/3  bg-nude p-8">
+      <ul class="space-y-4">
         <li>
-          <div class="flex gap-4 items-center mb-2">
+          <div class="flex gap-2 items-center mb-2">
             <img src="@/assets/check-circle-rounded.svg" alt="" />
-            <h4 class="font-bold text-lg">A Customized Experience for Every Style</h4>
+            <h4 class="font-bold text-xl">
+              A Customized Experience for Every Style
+            </h4>
           </div>
-          <p>
+          <p class="ml-8 font-light leading-[150%]">
             Our customizing tool allows you to filter by size, color, type of
             furniture and more!
           </p>
         </li>
         <li>
-          <div class="flex gap-4 items-center mb-2">
+          <div class="flex gap-2   items-center mb-2">
             <img src="@/assets/check-circle-rounded.svg" alt="" />
-            <h4 class="font-bold text-lg">Competitive Prices</h4>
+            <h4 class="font-bold text-xl">Competitive Prices</h4>
           </div>
-          <p>
+          <p class="ml-8 font-light leading-[150%]">
             The majority of our products are priced at under $1000 - some as low
             as $89!
           </p>

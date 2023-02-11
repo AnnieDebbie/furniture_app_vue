55728beaa5b3d10ff66fb61b00ee418e4252692e
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2023-02-11 12:47:20 +0100

    Style footer section Add more spacing Change font color and size of input element and button Display elements in a flex container

--- src/components/footer/FooterSection.vue
+++ src/components/footer/FooterSection.vue
@@ -1,24 +1,38 @@
 <template lang="">
-  <footer class="mt-4 bg-nude flex p-4">
-    <div>
-      <ul>
-        <li>About Us</li>
-        <li>Services</li>
-        <li>Blog</li>
-      </ul>
-    </div>
-    <div>
-      <ul>
-        <li>Contact Us</li>
-        <li>Support</li>
-        <li>Live chat</li>
-      </ul>
-    </div>
-    <div><input type="text" /><ButtonUI>Subscribe</ButtonUI></div>
-  </footer>
+  <section class="mt-10 bg-nude p-12">
+    <footer class="container mx-auto flex justify-between items-center">
+      <div>
+        <ul class="font-medium text-xl space-y-2">
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+      <div>
+        <ul class="font-medium text-xl space-y-2">
+          <li>Contact Us</li>
+          <li>Support</li>
+          <li>Live chat</li>
+        </ul>
+      </div>
+      <div>
+        <input
+          class="p-4 placeholder:text-black"
+          placeholder="Enter your email address"
+          type="text"
+          name=""
+          id=""
+        />
+
+        <ButtonUI class="rounded rounded-sm bg-black text-white p-4">
+          Subscribe to our newsletter
+        </ButtonUI>
+      </div>
+    </footer>
+  </section>
 </template>
 <script>
-import ButtonUI from '../UI/ButtonUI.vue';
+import ButtonUI from "../UI/ButtonUI.vue";
 export default {
   name: "FooterSection",
   components: { ButtonUI },
